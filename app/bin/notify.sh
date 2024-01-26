520fcd577256aafa78303356e987ddabf45631fa
Palleri <palleri@mario.se>
2024-01-26 07:36:44 +0100

    edit $host in notify.sh

--- app/bin/notify.sh
+++ app/bin/notify.sh
@@ -2,18 +2,18 @@
 
 
 date=$(date '+%Y-%m-%d' -d '+90 days')
-id=$(mysql -h db -u root --password=$password --database inventory -s -r -N --execute="SELECT id FROM stock WHERE exp_date <='$date' AND exp_date<>'' AND notify=0;")
+id=$(mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="SELECT id FROM stock WHERE exp_date <='$date' AND exp_date<>'' AND notify=0;")
 
 
 
 for i in $id
 do
-name=$(mysql -h db -u root --password=$password --database inventory -s -r -N --execute="SELECT name FROM stock WHERE id ='$id';")
-stock=$(mysql -h db -u root --password=$password --database inventory -s -r -N --execute="SELECT stock FROM stock WHERE id ='$id';")
-min=$(mysql -h db -u root --password=$password --database inventory -s -r -N --execute="SELECT min FROM stock WHERE id ='$id';")
-location=$(mysql -h db -u root --password=$password --database inventory -s -r -N --execute="SELECT location FROM stock WHERE id ='$id';")
-category=$(mysql -h db -u root --password=$password --database inventory -s -r -N --execute="SELECT category FROM stock WHERE id ='$id';")
-exp_date=$(mysql -h db -u root --password=$password --database inventory -s -r -N --execute="SELECT exp_date FROM stock WHERE id ='$id';")
+name=$(mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="SELECT name FROM stock WHERE id ='$id';")
+stock=$(mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="SELECT stock FROM stock WHERE id ='$id';")
+min=$(mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="SELECT min FROM stock WHERE id ='$id';")
+location=$(mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="SELECT location FROM stock WHERE id ='$id';")
+category=$(mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="SELECT category FROM stock WHERE id ='$id';")
+exp_date=$(mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="SELECT exp_date FROM stock WHERE id ='$id';")
 
 
 if [ -z "$id" ]
@@ -26,7 +26,7 @@ curl -k -X POST -H "Content-Type: application/json" \
 http://$hahost/api/services/shopping_list/add_item -d '{"name":"'"[BEREDSKAP] $name $stock/$min $category - $exp_date - $location"'"}'
 
 
-mysql -h db -u root --password=$password --database inventory -s -r -N --execute="UPDATE stock SET notify=1 WHERE id='$id';"
+mysql -h $host -u root --password=$password --database inventory -s -r -N --execute="UPDATE stock SET notify=1 WHERE id='$id';"
 
 
 fi
